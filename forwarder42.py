3001fd4fd78cde69798adce327b64595b93d0fb4
kmm <kmmiil95@gmail.com>
2020-07-11 04:29:47 +0300

    Initial commit

---
+++ forwarder42.py
@@ -0,0 +1,49 @@
+import asyncio
+
+from telethon import TelegramClient, events
+from telethon.events import NewMessage
+import yaml
+import logging
+
+loggingFormat = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=loggingFormat)
+logging.getLogger('telethon').setLevel(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+class Forwarder42:
+    channel_pairs = []
+    bot_name = "bot"
+
+    async def start(self, config):
+        self.channel_pairs = config["channel_pairs"]
+        self.bot_name = config["bot_name"]
+        bot = TelegramClient(config["bot_name"],
+                             config["api_id"],
+                             config["api_hash"])
+        await bot.start(bot_token=config["bot_token"])
+
+        pattern_to_match = f'(?i){bot_name}.+'
+
+        @bot.on(events.NewMessage(pattern=pattern_to_match))
+        async def handler(event: NewMessage.Event):
+            logger.info(event)
+            response = event.text.replace(f'@{bot_name}', "")
+
+            target_id = 0
+            for i in self.channel_pairs:
+                if i[0] == event.chat_id:
+                    target_id = i[1]
+
+            if target_id != 0:
+                await bot.send_message(target_id, response)
+
+        await bot.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    with open('config.yml', 'rb') as f:
+        config = yaml.safe_load(f)
+    logger.info("init")
+    forwarder = Forwarder42()
+    asyncio.run(forwarder.start(config))
---
+++ readme.md
@@ -0,0 +1,20 @@
+###The idea of this script is to forward messages from one Telegram group to another.  
+
+###Usage:  
+```
+Group 1  
+@your_bot_name text you want to forward  
+Group 2:  
+Your_bot_name: text you want to forward #bot will output this text
+```
+
+
+###Setup
+1) install python 3.6+
+2) run `make install` or `pip install -r requirements.txt`
+3) create a file named config.yml using sample_config.yml
+4) create a Telegram bot using botFather, paste the given bot token to the config.yml, also put the name of your bot there
+5) use botFather to turn off privacy of the bot
+6) obtain api keys from https://core.telegram.org/api/obtaining_api_id and paste them to config.yml
+7) get the group chat id's and place them into the config.yml as shown in the sample_config.yml
+8) run `make run` or `python forwarder42.py`
